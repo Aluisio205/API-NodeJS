5bebb9dd229a208b0041a85bf69c78b0da01d21b
Aluisio Pereira da Cruz Júnior <aluisio205@gmail.com>
2021-08-17 14:53:24 -0300

    att

---
+++ src/services/email-service.js
@@ -0,0 +1,17 @@
+// 'use strict'
+
+// const config = require('../config');
+// const sendgrid = require('@sendgrid/mail');
+// const sendgridKey = config.sendgridKey;
+
+
+
+
+// exports.send = async (to, subject, body) => {
+//     sendgrid.send({
+//         to: to,
+//         from: 'aluisio205@gmail.com',
+//         subject: subject,
+//         html: body
+//     });
+// };
