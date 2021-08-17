5fcc3ddad1924639c4ca1188af1678698da55f3b
Aluisio Pereira da Cruz Júnior <aluisio205@gmail.com>
2021-08-17 15:47:23 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ global.SALT_KEY = 'f5b99242-6504-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong> , seja bem vindo ao NODEJS';
 
 module.exports = {
-    connectionString: 'mongodb+srv://sa:123@ndstr.lwwcu.mongodb.net/myFirstDatabase',
+    connectionString: 'YOUR_CONNECTION_STRING',
     containerConnection: 'TBD'
 }
 
